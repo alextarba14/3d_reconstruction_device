5c77eb37b28c016f3e6be310df4c975892e80b85
Tarba Lucian-Alexandru <alextarba14@gmail.com>
2021-01-09 17:28:30 +0200

    Script to display information from IMU

---
+++ gyro_data.py
@@ -0,0 +1,21 @@
+
+import numpy as np
+import pyrealsense2 as rs
+
+# Configure depth and color streams
+pipeline = rs.pipeline()
+config = rs.config()
+config.enable_stream(rs.stream.accel, rs.format.motion_xyz32f, 250)
+config.enable_stream(rs.stream.gyro, rs.format.motion_xyz32f, 200)
+
+# Start streaming
+pipeline.start(config)
+
+while True:
+    frames = pipeline.wait_for_frames()
+    for frame in frames:
+        motion_data = frame.as_motion_frame().get_motion_data()
+        print(motion_data)
+        # prints: x: -0.0294199, y: -7.21769, z: -6.41355 for me
+        # to get numpy array:
+        print(np.array([motion_data.x, motion_data.y, motion_data.z]))
