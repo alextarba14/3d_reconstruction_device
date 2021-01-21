01477fc0032c9a92c8aac6a7a8b01e68e39ad5f5
Tarba Lucian-Alexandru <alextarba14@gmail.com>
2021-01-21 15:06:36 +0200

    Returning theta for both functions in order to be used in another file.

--- motion/motion.py
+++ motion/motion.py
@@ -1,11 +1,8 @@
 import threading
 from angle import Angle
-import numpy as np
-import pyrealsense2 as rs
 import math
-from timeit import default_timer as timer
 
-# Global variables
+# Global variables for motion
 alpha = 0.98
 first = True
 theta = Angle(0, 0, 0)
@@ -53,7 +50,7 @@ def process_accel(accel_data):
         theta.x = theta.x * alpha + accel_angle.x * (1-alpha)
         theta.z = theta.z * alpha + accel_angle.z * (1-alpha)
     mutex.release()
-    print("Theta angle(accel): " + str(theta))
+    return theta
 
 
 def process_gyro(gyro_data, timestamp):
@@ -76,66 +73,17 @@ def process_gyro(gyro_data, timestamp):
     # gyro_data.y : Yaw
     # gyro_data.z : Roll
     gyro_angle = Angle(gyro_data.x,gyro_data.y,gyro_data.z)
-    print("Gyro angle (before): " + str(gyro_angle))
     # Compute the difference between arrival times of previous and current gyro frames
     dt_gyro = (timestamp - last_timestamp_gyro) / 1000.0
     last_timestamp_gyro = timestamp
-    print("dt_gyro: " + str(dt_gyro))
     # Change in angle equals gyro measurements * time passed since last measurement
     gyro_angle = gyro_angle * dt_gyro
 
-    print("Gyro angle(after): " + str(gyro_angle))
-
     # Apply the calculated change of angle to the current angle (theta)
     global mutex
+    global theta
     mutex.acquire()
     theta.add(-gyro_angle.z,-gyro_angle.y,gyro_angle.x)
     mutex.release()
-    print("Theta angle(gyro): " + str(theta))
+    return theta
 
-
-
-def main():
-    start = timer()
-    nr = 0
-    # Configure gyro and accelerometer streams
-    pipeline = rs.pipeline()
-    config = rs.config()
-    # Configuring streams at different rates
-    # Accelerometer available FPS: {63, 250}Hz
-    # Gyroscope available FPS: {200,400}Hz
-    config.enable_stream(rs.stream.accel, rs.format.motion_xyz32f, 250)
-    config.enable_stream(rs.stream.gyro, rs.format.motion_xyz32f, 200)
-
-    # Start streaming
-    pipeline.start(config)
-
-    while True:
-        frames = pipeline.wait_for_frames()
-        for frame in frames:
-            motion = frame.as_motion_frame()
-            if motion and motion.get_profile().stream_type() == rs.stream.accel:
-                # Accelerometer frame
-                # Get accelerometer measurements
-                accel_data = motion.get_motion_data()
-                print("Accel data: " + str(accel_data))
-                process_accel(accel_data)
-            elif motion and motion.get_profile().stream_type() == rs.stream.gyro:
-                # Gyro frame
-                # Get the timestamp of current frame
-                timestamp = motion.get_timestamp()
-                # Get gyro measurements
-                gyro_data = motion.get_motion_data()
-                print("gyro_data : " + str(gyro_data))
-                process_gyro(gyro_data, timestamp)
-        nr = nr + 1
-        end = timer()
-        if end - start >= 5:
-            print("Stopped! Nr: " + str(nr))
-            break
-
-
-
-
-if __name__ == "__main__":
-    main()
