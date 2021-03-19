8262566afbada17a0d84108cc42be75295d75b1a
Tarba Lucian-Alexandru <alextarba14@gmail.com>
2021-03-19 19:34:46 +0200

    @wip applying transformations to pointclouds.

--- mathematics/transformations.py
+++ mathematics/transformations.py
@@ -13,7 +13,6 @@ def apply_transform(pointcloud, matrix):
 def apply_transformations(pointclouds, transf_matrices, length, matrix_decrement):
     # number of transformation matrices: length + 1 - first one is empty
     # a matrix has 4 lines => no_lines=  4*(length+1)
-    matrix_index = len(transf_matrices)
 
     # initial pointcloud + length
     pointcloud_index = len(pointclouds)
@@ -21,17 +20,34 @@ def apply_transformations(pointclouds, transf_matrices, length, matrix_decrement
 
     index = length
     while index > 0:
-        current_transf_matrix = transf_matrices[matrix_index - matrix_decrement:matrix_index]
-        inverse_transf_matrix = np.linalg.inv(current_transf_matrix)
-
         current_pointcloud = pointclouds[pointcloud_index - pointcloud_length:pointcloud_index]
-        # previous_pointcloud = pointclouds[pointcloud_index - 2 * pointcloud_length:pointcloud_index - pointcloud_length]
 
         # append 1 at the end
         ones = np.ones((pointcloud_length, 1), dtype=np.float32)
         current_pointcloud = np.append(current_pointcloud, ones, axis=1)
 
+        # apply transformations for each pointcloud
+        j = index
+        matrix_index = (index + 1) * matrix_decrement
+        while j > 0:
+            current_transf_matrix = transf_matrices[matrix_index - matrix_decrement:matrix_index]
+            inverse_transf_matrix = np.linalg.inv(current_transf_matrix)
+            count = 0
+            for i in range(len(current_pointcloud)):
+                if current_pointcloud[i][0] != 0 and current_pointcloud[i][1] != 0 and current_pointcloud[i][2]!=0:
+                    # avoid multiplication by zero
+                    current_pointcloud[i] = current_pointcloud[i].dot(inverse_transf_matrix)
+
+            j = j - 1
+            matrix_index = matrix_index - matrix_decrement
+
+        current_pointcloud = np.delete(current_pointcloud, 3, axis=1)
+        # update the pointcloud in memory
+        pointclouds[pointcloud_index - pointcloud_length:pointcloud_index] = current_pointcloud
+
         # decrement indexes
         index = index - 1
         matrix_index = matrix_index - matrix_decrement
         pointcloud_index = pointcloud_index - pointcloud_length
+
+    return pointclouds
