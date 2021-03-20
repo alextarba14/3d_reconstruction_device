c0af69cd00d09b27fd000900d9a1856ff773bcf4
Tarba Lucian-Alexandru <alextarba14@gmail.com>
2021-03-20 22:06:13 +0200

    @fix inverting matrices only once in point_cloud.py and refactored to use the list approach.

--- mathematics/transformations.py
+++ mathematics/transformations.py
@@ -19,50 +19,48 @@ def apply_transformations(pointclouds, transf_matrices, length, matrix_decrement
     # a matrix has 4 lines => no_lines=  4*(length+1)
 
     # initial pointcloud + length
-    pointcloud_index = len(pointclouds)
-    pointcloud_length = (int)(pointcloud_index / (length + 1))
+    pointcloud_index = len(pointclouds) - 1
 
     index = length
     while index > 0:
         start_time_index = time.time()
         print(f'Started index: {index} at: ', start_time_index)
-        current_pointcloud = pointclouds[pointcloud_index - pointcloud_length:pointcloud_index]
+        current_pointcloud = pointclouds[pointcloud_index]
+        pc_length = len(current_pointcloud)
 
         # append 1 at the end
-        ones = np.ones((pointcloud_length, 1), dtype=np.float32)
+        ones = np.ones((pc_length, 1), dtype=np.float32)
         current_pointcloud = np.append(current_pointcloud, ones, axis=1)
 
         # apply transformations for each pointcloud
         j = index
-        matrix_index = (index + 1) * matrix_decrement
         while j > 0:
             start_time = time.time()
             print(f'Started j: {j} at: ', start_time)
-            current_transf_matrix = transf_matrices[matrix_index - matrix_decrement:matrix_index]
-            inverse_transf_matrix = np.linalg.inv(current_transf_matrix)
-            count = 0
-            for i in range(len(current_pointcloud)):
-                if current_pointcloud[i][0] != 0 and current_pointcloud[i][1] != 0 and current_pointcloud[i][2] != 0:
+            current_transf_matrix = transf_matrices[j]
+
+            for i in range(pc_length):
+                if current_pointcloud[i][2] != 0:
                     # avoid multiplication by zero
-                    current_pointcloud[i] = current_pointcloud[i].dot(inverse_transf_matrix)
+                    current_pointcloud[i] = current_pointcloud[i].dot(current_transf_matrix)
 
             j = j - 1
-            matrix_index = matrix_index - matrix_decrement
             print(f'Stopped j: {j} after: ', time.time() - start_time)
 
+        # removing the last column since it was added to perform dot product between vector[1x(3+1)] and transform matrix[4x4]
         current_pointcloud = np.delete(current_pointcloud, 3, axis=1)
         # update the pointcloud in memory
-        pointclouds[pointcloud_index - pointcloud_length:pointcloud_index] = current_pointcloud
+        pointclouds[pointcloud_index] = current_pointcloud
 
         # save the transformed pointcloud
         file_name = f'transformed{index}.ply'
         export_numpy_array_to_ply(current_pointcloud, file_name=file_name)
 
         print(f'Stopped index: {index} after: ', time.time() - start_time_index)
+
         # decrement indexes
         index = index - 1
-        matrix_index = matrix_index - matrix_decrement
-        pointcloud_index = pointcloud_index - pointcloud_length
+        pointcloud_index = pointcloud_index - 1
 
     print("Stopped after: ", time.time() - start_time_function)
     exit(1)
