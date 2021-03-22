43f4ee901aa6abe75fc71de7ddfc8d54f9f71739
Tarba Lucian-Alexandru <alextarba14@gmail.com>
2021-03-22 23:01:29 +0200

    Writing to file  the texture information in BGR format.

--- export/ply.py
+++ export/ply.py
@@ -1,18 +1,25 @@
 import numpy as np
 import pyrealsense2 as rs
 
-def export_numpy_array_to_ply(array, file_name="test.ply"):
+def export_numpy_array_to_ply(array, texture, file_name="test.ply"):
     # transpose numpy array in order to put columns as lines
     transposed_array = array.transpose()
     # get the z column - depth
     transposed_depth = transposed_array[2]
     # keep only elements that have depth greater than 0
     indexes = np.where(transposed_depth>0)
+
+    # update both arrays to keep only valid point information
     array = array[indexes]
+    texture =  texture[indexes]
 
     # multiply y and z columns in order to rotate them to be displayed properly
     array[:,1] *= -1
     array[:,2] *= -1
+
+
+    # concatenate both vertices and texture in one array with (length, 5) shape
+    array = np.concatenate((array, texture), axis=1)
 
     # construct the header
     length = len(array)
@@ -23,6 +30,9 @@ element vertex {length}
 property float32 x
 property float32 y
 property float32 z
+property uchar blue
+property uchar green
+property uchar red
 end_header""".format(length=length)
 
     np.savetxt(file_name, array, fmt="%.6g", header=header, delimiter=" ", comments="")
@@ -36,6 +46,7 @@ def default_export_points(points, file_name="default.ply"):
     # In this example we'll generate a textual PLY with normals (mesh is already created by default)
     ply.set_option(rs.save_to_ply.option_ply_binary, False)
     ply.set_option(rs.save_to_ply.option_ply_normals, True)
+    ply.set_option(rs.save_to_ply.option_ignore_color, False)
 
     # Apply the processing block to the frameset which contains the depth frame and the texture
     ply.process(points)
