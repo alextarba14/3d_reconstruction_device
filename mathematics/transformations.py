ae694ffaf4bf6daaae9d88878e9724130763522d
Tarba Lucian-Alexandru <alextarba14@gmail.com>
2021-04-23 22:15:37 +0300

    @wip added method to remove points that have less neighbours in a given radius.

--- mathematics/transformations.py
+++ mathematics/transformations.py
@@ -1,14 +1,32 @@
 import numpy as np
+import scipy.spatial as spatial
+
 
 def remove_points_far_away_from_centroid(points, cutoff=1.0):
     # find the points` centroid
     centroid = np.mean(points, axis=0)
     distances = np.empty(len(points))
     for i in range(len(points)):
+        # exclude points that have zero depth
         if points[i][2] != 0:
             dist = np.linalg.norm(centroid - points[i])
             distances[i] = dist
 
-    # keep only points that are under the threshold
-    indices = distances < cutoff
-    return indices
+    # keep only points that are under the cutoff value
+    return distances < cutoff
+
+
+def remove_points_with_less_neighbours(points, nb_neighbours, radius=0.03):
+    tree = spatial.cKDTree(points)
+    neighbours = np.zeros(len(points))
+    for i in range(len(points)):
+        # exclude points that have zero depth
+        if points[i][2] != 0:
+            if neighbours[i] == 0:
+                nearest = tree.query_ball_point(points[i], radius)
+                if len(nearest) > nb_neighbours:
+                    neighbours[nearest] = 1
+
+    # keep only points that have no. of neighbours above the threshold
+    return  neighbours == 1
+
