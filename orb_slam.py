c0e3139a6e1776803f848e3a24f8a8194360e331
Tarba Lucian-Alexandru <alextarba14@gmail.com>
2021-05-21 09:10:54 +0300

    @wip integrating feature matching and computing transformation matrix using normalize method.

--- orb_slam.py
+++ orb_slam.py
@@ -2,8 +2,11 @@ import pyrealsense2 as rs
 import numpy as np
 import cv2
 
-from features.features import match_frames_FLANN_angles, compute_K_from_intrinsics, normalize
+from features.features import match_frames_FLANN_angles, compute_K_from_intrinsics, normalize, get_correspondences
+from input_output.ply import export_numpy_array_to_ply
+from mathematics.matrix import get_indexes_of_valid_points
 from processing.icp import best_fit_transform
+from processing.process import get_texture_for_pointcloud
 
 pipeline = rs.pipeline()
 config = rs.config()
@@ -31,10 +34,16 @@ cfg = pipeline.start(config)
 align_to = rs.stream.color
 align = rs.align(align_to)
 
-profile = cfg.get_stream(rs.stream.depth)
-intrinsics = profile.as_video_stream_profile().get_intrinsics()
+# Processing blocks
+pc = rs.pointcloud()
+decimate = rs.decimation_filter()
+decimate.set_option(rs.option.filter_magnitude, 3)
+colorizer = rs.colorizer()
+
+profile=  cfg.get_stream(rs.stream.color)
+intr_color = profile.as_video_stream_profile().get_intrinsics()
 # compute intrinsics matrix
-K = compute_K_from_intrinsics(intrinsics)
+K = compute_K_from_intrinsics(intr_color)
 K_inv = np.linalg.inv(K)
 
 # Getting the depth sensor's depth scale (see rs-align example for explanation)
@@ -43,6 +52,11 @@ depth_scale = depth_sensor.get_depth_scale()
 
 index = 0
 count = 0
+I = np.eye(4, 4)
+transf_matrices = []
+vertices_array = []
+texture_data_array = []
+tex_coords_array = []
 try:
     while True:
         # Wait for a coherent pair of frames: depth and color
@@ -63,44 +77,69 @@ try:
         depth_image = np.asanyarray(depth_frame.get_data())
         color_image = np.asanyarray(color_frame.get_data())
 
-        if index % 5 == 0:
-            if count == 0:
-                # first frame
-                prev_color_frame = color_image
-                prev_depth_frame = depth_image
-            else:
+        mapped_frame, color_source = color_frame, color_image
+
+        depth_frame = decimate.process(depth_frame)
+        points = pc.calculate(depth_frame)
+        pc.map_to(mapped_frame)
+
+        # Pointcloud data to arrays
+        v, t = points.get_vertices(), points.get_texture_coordinates()
+        verts = np.asanyarray(v).view(np.float32).reshape(-1, 3)  # xyz
+        texcoords = np.asanyarray(t).view(np.float32).reshape(-1, 2)  # uv
+
+        if index == 0:
+            # keep information to get color for each frame only once since they are all the same
+            color_w, color_h = color_frame.get_width(), color_frame.get_height()
+            bytes_per_pixel = color_frame.get_bytes_per_pixel()
+            stride_in_bytes = color_frame.get_stride_in_bytes()
+
+            # first frame
+            prev_color_frame = color_image
+            prev_depth_frame = depth_image
+
+            index = index + 1
+            continue
+
+        if index % 10 == 0:
+            valid_points = get_indexes_of_valid_points(verts)
+            verts = verts[valid_points]
+
+            texture_data = np.asanyarray(color_frame.get_data()).view(np.uint8).reshape(-1, 1)
+            vertices_array.append(verts)
+            texture_data_array.append(texture_data)
+            tex_coords_array.append(texcoords[valid_points])
+            try:
                 # current frame is color_image
-                try:
-                    pts_A, pts_B = match_frames_FLANN_angles(prev_color_frame, color_image)
-                    # at least three correspondences found
-                    # get depth points associated
-                    depth_prev = np.array([prev_depth_frame[y, x] for x, y in pts_A]) * depth_scale
-                    depth_curr = np.array([depth_image[y, x] for x, y in pts_B]) * depth_scale
+                pts_A, pts_B = get_correspondences(prev_color_frame, color_image)
+                # at least three correspondences found
+                # get depth points associated
+                depth_prev = np.array([prev_depth_frame[y, x] for x, y in pts_A]) * depth_scale
+                depth_curr = np.array([depth_image[y, x] for x, y in pts_B]) * depth_scale
 
-                    # normalize points
-                    pts_A = normalize(pts_A, K_inv)
-                    pts_B = normalize(pts_B, K_inv)
+                # normalize points
+                pts_A = normalize(pts_A, K_inv)
+                pts_B = normalize(pts_B, K_inv)
 
-                    # create 3 dimensional array with x,y,z coordinates
+                # remove zero depth points from both
+                valid_a = [depth_prev > 0]
+                valid_b = [depth_curr > 0]
 
-                    print("Before removing: ", len(pts_A))
+                # need valid correspondences in both point sets
+                valid = valid_a and valid_b
 
-                    # remove zero depth points from both
-                    valid_a = [depth_prev > 0]
-                    valid_b = [depth_curr > 0]
+                # create 3 dimensional array with x,y,z coordinates
+                pts_A = np.column_stack((pts_A, depth_prev))[valid]
+                pts_B = np.column_stack((pts_B, depth_curr))[valid]
 
-                    # need valid correspondences in both point sets
-                    valid = valid_a and valid_b
+                print("Matches: ", len(pts_A))
+                T = best_fit_transform(pts_B, pts_A)
+                print(T)
+            except ValueError as e:
+                print(e)
+                T = I
 
-                    pts_A = np.column_stack((pts_A, depth_prev))[valid]
-                    pts_B = np.column_stack((pts_B, depth_curr))[valid]
-
-                    print("Matches: ", len(pts_A))
-                    T = best_fit_transform(pts_A, pts_B)
-                    print(T)
-                except ValueError as e:
-                    print(e)
-
+            transf_matrices.append(T)
             prev_color_frame = color_image
             prev_depth_frame = depth_image
             count = count + 1
@@ -114,7 +153,55 @@ try:
             break
 
         index = index + 1
+        if count == 15:
+            break
 finally:
     # Stop streaming
     pipeline.stop()
 
+cv2.destroyAllWindows()
+main_color = get_texture_for_pointcloud(vertices_array[0].copy(), tex_coords_array[0], texture_data_array[0], color_w,
+                                        color_h,
+                                        bytes_per_pixel, stride_in_bytes)
+for i in range(1, len(vertices_array)):
+    # get color for current point cloud
+    current_color = get_texture_for_pointcloud(vertices_array[i].copy(), tex_coords_array[i], texture_data_array[i],
+                                               color_w, color_h,
+                                               bytes_per_pixel, stride_in_bytes)
+    # append color to the main color array
+    main_color = np.vstack((main_color, current_color))
+
+main_pc = vertices_array[0]
+index = 1
+length = len(vertices_array)
+# apply transformation to align each point cloud over the first point cloud
+while index < length:
+    current_pc = vertices_array[index]
+
+    # append a column of ones to transform the current point cloud
+    # in homogeneous coordinates to do matrix multiplication
+    ones = np.ones((len(current_pc), 1))
+    current_pc = np.append(current_pc, ones, axis=1)
+
+    # transpose current point cloud to allow matrix multiplication
+    current_pc = current_pc.T
+
+    # apply all transformation matrices to current point cloud
+    j = 1
+    while j <= index:
+        current_transf = transf_matrices[j]
+        current_pc = current_transf @ current_pc
+        j = j + 1
+
+    # transpose current point cloud back to have info in xyz coordinates
+    current_pc = current_pc.T
+    # remove column used to allow matrix multiplication
+    current_pc = np.delete(current_pc, 3, axis=1)
+
+    # append current point cloud to the main point cloud
+    main_pc = np.vstack((main_pc, current_pc))
+
+    # move to the next point cloud
+    index = index + 1
+
+export_numpy_array_to_ply(main_pc, main_color, "./demo/result.ply")
